af26a2e3111f70642f1cc3f041b1b1fa0602c2bc
tom <tom@daoshenggroup.com>
2016-12-16 15:07:16 +0800

    init

---
+++ uics-asset-api/src/main/java/com/uics/asset/service/AccountService.java
@@ -0,0 +1,10 @@
+package com.uics.asset.service;
+
+import com.uics.asset.entity.Account;
+
+/**
+ * @author baiqw
+ */
+public interface AccountService {
+    Account signIn(Account account);
+}
---
+++ uics-asset-service-provider/src/main/java/com/uics/asset/service/AccountServiceImpl.java
@@ -0,0 +1,13 @@
+package com.uics.asset.service;
+
+import com.uics.asset.entity.Account;
+
+/**
+ * @author baiqw
+ */
+public class AccountServiceImpl implements AccountService {
+    @Override
+    public Account signIn(Account account) {
+        return null;
+    }
+}
